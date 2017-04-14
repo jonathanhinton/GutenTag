e638bbef92312313606864d1a755987c55f844f7
jonathanhinton <hintonjonathan@gmail.com>
2017-04-14 16:16:51 -0500

    set up basic user input

--- app/app.js
+++ app/app.js
@@ -1,1 +1,23 @@
 var app = angular.module('gutenTag', ['ngAnimate', 'ngAria', 'ngRoute', 'ngMaterial']);
+
+app.controller("GutenCtrl", function () {
+
+    var self = this;
+
+    self.showPopup = function () {
+        window.alert();
+    };
+    self.Options = [];
+
+    self.AddOption = function (e) {
+        // Get value from input field
+        var input_val = $("#firstoption").val();
+        var incrementer = 1;
+        self.Options.push(input_val);
+        console.log("Options", self.Options);
+        $("#firstoption").val("");
+        // Then append it onto the Options object
+        e.preventDefault();
+
+    };
+});
